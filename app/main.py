80f44c86472920060ee2c5aacf23416178df13c6
Vittorio Rivabella <20019426@uhi.ac.uk>
2021-01-18 12:51:18 +0100

    adds server and GANs Files

---
+++ app/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify, url_for, send_file
+from flask_cors import CORS
+import subprocess
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = "../datasets/uploads/test"
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+    
+ALLOWED_EXTENSIONS = set(["jpg", "png", "jpeg"])
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CORS(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    print("starts")
+    if request.method == 'GET':
+            return send_file("..\\results\\human_pix2pix\\test_latest\\images\\img_fake_B.png", mimetype='image/jpeg')
+
+
+    if request.method == 'POST':
+        #check if file is in post request
+        print(request.files)
+        if 'img' not in request.files:
+            print('No file part')
+            return jsonify({"result" : 1})
+        file = request.files['img']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        print("found")
+
+        if file.filename == '':
+           print('No selected file')
+           return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # Create renaming system;
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+<<<<<<< HEAD
+        # TODO: Get rid of subprocess for resize-image
+        subprocess.run(["python", "resize-image.py"])
+        print("resize done")
+        # TODO: add some comments here as well
+=======
+        subprocess.run(["python", "resize-image.py"])
+        print("resize done")
+>>>>>>> 5ab65389e1a198dcffe8e0a5caa63f88d5ac17a9
+        subprocess.run(["python", "./test.py", "--dataroot", "./datasets/uploads", "--name",  "human_pix2pix", "--model", "pix2pix", "--direction", "BtoA", "--batch_size", "256", "--gpu_ids", "-1"], cwd = "C:\\Users\\smile\\00_repos\\susanna-server\\" )
+
+    return jsonify({"process": "ok"})
+# @app.route("/get-prediction/<imgName>")
+
+
+if __name__ == "__main__":
+    app.run(debug = true)
---
+++ app/resize-image.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import os
+
+<<<<<<< HEAD
+# TODO: comment function
+=======
+
+>>>>>>> 5ab65389e1a198dcffe8e0a5caa63f88d5ac17a9
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[0]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor      
+        
+
+        return ver
+
+
+
+
+if __name__ == "__main__":
+    img = cv2.imread("../datasets/uploads/test/img.jpeg", 3);
+    whiteblankimage = 255 * np.ones(shape=[512, 512, 3], dtype=np.uint8);
+    stacked = stackImages(1,([whiteblankimage, img]));
+    outputName = "../datasets/uploads/test/" + "img" + ".jpeg"
+        # print(outputName)
+            
+    cv2.imwrite(outputName, stacked);
